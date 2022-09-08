a7d250c1248d6864991021d02e51ee662652458b
Andrés Sanabria <jandrev1312@gmail.com>
2022-09-08 11:36:28 -0500

    more mdx bug fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import mdx from "@astrojs/mdx";
+import tailwind from "@astrojs/tailwind"
+import { defineConfig } from "astro/config"
 
-import image from "@astrojs/image";
+import image from "@astrojs/image"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx(), image()],
+  integrations: [tailwind(), image()],
   ssr: {
-    external: ["svgo"]
-  }
-});
+    external: ["svgo"],
+  },
+})
