aba89a02d7a1b79dd8d45d452dac444f926e89b8
Andrés Sanabria <jandrev1312@gmail.com>
2022-10-09 13:32:09 -0500

    Added umami analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
-import react from "@astrojs/react"
-import tailwind from "@astrojs/tailwind"
-import { defineConfig } from "astro/config"
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), partytown()],
   ssr: {
-    external: ["svgo"],
-  },
-})
+    external: ["svgo"]
+  }
+});
